ae363f346907bbeeaca0498061de3d3f3d3ce87f
Lelebanimes00 <94663940+CORINGA88hacker@users.noreply.github.com>
2025-08-10 00:41:47 -0300

    Update and rename README.md to animes.js

---
+++ animes.js
@@ -0,0 +1,87 @@
+[
+  {
+    "id": "01",
+    "nome": "One Piece",
+    "data": "1999-10-20",
+    "avaliacao": 8.8,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/6/65/One_Piece_Logo.png"
+  },
+  {
+    "id": "02",
+    "nome": "Naruto",
+    "data": "2002-10-03",
+    "avaliacao": 8.1,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg"
+  },
+  {
+    "id": "03",
+    "nome": "Attack on Titan",
+    "data": "2013-04-07",
+    "avaliacao": 9.0,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/0/0d/Attack_on_Titan_S1_cover.jpg"
+  },
+  {
+    "id": "04",
+    "nome": "Fullmetal Alchemist: Brotherhood",
+    "data": "2009-04-05",
+    "avaliacao": 9.2,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/7/7f/Fullmetal_Alchemist_Brotherhood_Volume_1.jpg"
+  },
+  {
+    "id": "05",
+    "nome": "Death Note",
+    "data": "2006-10-04",
+    "avaliacao": 8.7,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/6/6f/Death_Note_Vol_1.png"
+  },
+  {
+    "id": "06",
+    "nome": "Dragon Ball Z",
+    "data": "1989-04-26",
+    "avaliacao": 8.6,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/7/75/Dragon_Ball_Z_English_DVD_cover.jpg"
+  },
+  {
+    "id": "07",
+    "nome": "My Hero Academia",
+    "data": "2016-04-03",
+    "avaliacao": 8.0,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/1/1a/My_Hero_Academia_Volume_1_cover.jpg"
+  },
+  {
+    "id": "08",
+    "nome": "Sword Art Online",
+    "data": "2012-07-08",
+    "avaliacao": 7.5,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/7/7a/Sword_Art_Online_light_novel_volume_1_cover.jpg"
+  },
+  {
+    "id": "09",
+    "nome": "Demon Slayer",
+    "data": "2019-04-06",
+    "avaliacao": 8.7,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/a/a5/Demon_Slayer_Volume_1_cover.jpg"
+  },
+  {
+    "id": "10",
+    "nome": "Tokyo Ghoul",
+    "data": "2014-07-04",
+    "avaliacao": 7.8,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/4/49/Tokyo_Ghoul_volume_1_cover.jpg"
+  },
+  {
+    "id": "11",
+    "nome": "Mob Psycho 100",
+    "data": "2016-07-12",
+    "avaliacao": 8.6,
+    "capa": "https://upload.wikimedia.org/wikipedia/en/f/f6/Mob_Psycho_100_Volume_1_Cover.jpg"
+  },
+  {
+    "id": "12",
+    "nome": "Lord of Mysteries",
+    "data": "2025-06-28",
+    "avaliacao": "8,7",
+    "capa": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/c8fHePq3yTn3WvZd4hupkHwsjm5.jpg"
+  },
+]
+
